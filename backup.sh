58218c3b8d0940904a77b0dcba484efa6c16fd9f
nvasilie <nvasilie@vasilie166009AL.local>
2016-12-12 17:43:09 +0200

    Add backup.sh and .gitignore

---
+++ backup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+DBNAME=""
+EXPIRATION="1"
+Green='\033[0;32m'
+EC='\033[0m' # reset color
+FILENAME=`date +%H_%M_%d%m%Y`
+
+# terminate script on any fails
+set -e
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -exp|--expiration)
+    EXPIRATION="$2"
+    shift
+    ;;
+    -db|--dbname)
+    DBNAME="$2"
+    shift
+    ;;
+esac
+shift
+done
+
+if [[ -z "$DBNAME" ]]; then
+  echo "Missing DBNAME variable"
+  exit 1
+fi
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+  echo "Missing AWS_ACCESS_KEY_ID variable"
+  exit 1
+fi
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo "Missing AWS_SECRET_ACCESS_KEY variable"
+  exit 1
+fi
+if [[ -z "$AWS_DEFAULT_REGION" ]]; then
+  echo "Missing AWS_DEFAULT_REGION variable"
+  exit 1
+fi
+if [[ -z "$S3_BUCKET_PATH" ]]; then
+  echo "Missing S3_BUCKET_PATH variable"
+  exit 1
+fi
+if [[ -z "$DBURL_FOR_BACKUP" ]]; then
+  echo "Missing DBURL_FOR_BACKUP variable"
+  exit 1
+fi
+
+printf "${Green}Start dump${EC}"
+time pg_dump $DBURL_FOR_BACKUP | gzip >  /tmp/"${DBNAME}_${FILENAME}".gz
+
+printf "${Green}Move dump to AWS${EC}"
+time /app/vendor/awscli/bin/aws s3 cp /tmp/"${DBNAME}_${FILENAME}".gz s3://$S3_BUCKET_PATH/$DBNAME/"${DBNAME}_${FILENAME}".gz
+
+# cleaning after all
+rm -rf /tmp/"${DBNAME}_${FILENAME}".gz
