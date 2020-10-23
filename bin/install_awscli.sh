c69bee35f2293702b66bad977cd39bbcab91e272
Tomasz Kapelak <tom@nozbe.com>
2020-10-23 11:46:43 +0200

    Update install_awscli.sh

--- bin/install_awscli.sh
+++ bin/install_awscli.sh
@@ -4,15 +4,15 @@ chmod +x /app/vendor/awscli-bundle/install
 /app/vendor/awscli-bundle/install -i $INSTALL_DIR
 chmod u+x $INSTALL_DIR/bin/aws
 
-mkdir ~/.aws
+#mkdir ~/.aws
 
-cat >> ~/.aws/credentials << EOF
-[default]
-aws_access_key_id = $AWS_KEY
-aws_secret_access_key = $AWS_SECRET_KEY
-EOF
+#cat >> ~/.aws/credentials << EOF
+#[default]
+#aws_access_key_id = $AWS_KEY
+#aws_secret_access_key = $AWS_SECRET_KEY
+#EOF
 
-cat >> ~/.aws/config << EOF 
-[default]
-region = $AWS_REGION
-EOF
+#cat >> ~/.aws/config << EOF 
+#[default]
+#region = $AWS_REGION
+#EOF
