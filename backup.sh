0b76e4e316449b3676763fac635fa603344f0cba
runephilosof-abtion <57357936+runephilosof-abtion@users.noreply.github.com>
2021-06-09 12:31:56 +0200

    Style the output
    Hide unneeded info from gpg
    Handle warning from mysqldump about passing password (it was not insecure before, since it is being run on a single user temporary Heroku dyno).

--- backup.sh
+++ backup.sh
@@ -39,10 +39,11 @@ if [[ -z "$DB_BACKUP_GPG_PUB_KEY" ]]; then
   exit 1
 fi
 
+printf "${Green}Import GPG key${EC}\n"
 echo "$DB_BACKUP_GPG_PUB_KEY" > gpg-pubkey
-gpg --import gpg-pubkey
+gpg --quiet --import gpg-pubkey
 
-printf "${Green}Start dump${EC}"
+printf "${Green}Start dump${EC}\n"
 if [[ $DATABASE_URL = mysql* ]]; then
   TMP_BACKUP=/tmp/"${DB_BACKUP_FILENAME}_mysql_${DATE}".gpg
 
@@ -50,7 +51,13 @@ if [[ $DATABASE_URL = mysql* ]]; then
   DB_BACKUP_PASSWORD=$(echo $DATABASE_URL | cut -d: -f3 | cut -d@ -f1)
   DB_BACKUP_HOST=$(echo $DATABASE_URL | cut -d@ -f2 | cut -d: -f1)
   DB_BACKUP_DATABASE=$(echo $DATABASE_URL | cut -d/ -f4)
-  mysqldump -h $DB_BACKUP_HOST -p$DB_BACKUP_PASSWORD -u$DB_BACKUP_USER $DB_BACKUP_DATABASE | gpg --encrypt --recipient "$DB_BACKUP_GPG_PUB_KEY_ID" --output "$TMP_BACKUP" --trust-model always
+  
+  cat > .my.cnf <<-EOF
+    [client]
+    password=${DB_BACKUP_PASSWORD}
+EOF
+  chmod 600 .my.cnf
+  mysqldump -h $DB_BACKUP_HOST -u$DB_BACKUP_USER $DB_BACKUP_DATABASE | gpg --encrypt --recipient "$DB_BACKUP_GPG_PUB_KEY_ID" --output "$TMP_BACKUP" --trust-model always
 elif [[ $DATABASE_URL = postgres* ]]; then
   TMP_BACKUP=/tmp/"${DB_BACKUP_FILENAME}_postgresql_${DATE}".gpg
 
@@ -60,7 +67,7 @@ else
   exit 1;
 fi;
 
-printf "${Green}Move dump to AWS${EC}"
+printf "${Green}Move dump to AWS${EC}\n"
 AWS_ACCESS_KEY_ID=$DB_BACKUP_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$DB_BACKUP_AWS_SECRET_ACCESS_KEY /app/vendor/bin/aws --region $DB_BACKUP_AWS_DEFAULT_REGION s3 cp "$TMP_BACKUP" s3://$DB_BACKUP_S3_BUCKET_PATH/
 
 # cleaning after all
